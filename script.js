4ee1bfc98c2be4cec996401deddb5b0afb5bcdf7
Mansa7705-k <1012412107@despu.edu.in>
2026-01-08 20:34:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+
 document.getElementById("time").innerHTML =
     "Current Date & Time: " + new Date().toLocaleString();
+
+
+const images = [
+    "https://picsum.photos/400/200?1",
+    "https://picsum.photos/400/200?2",
+    "https://picsum.photos/400/200?3",
+    "https://picsum.photos/400/200?4",
+    "https://picsum.photos/400/200?5"
+];
+
+let index = 0;
+const carouselImage = document.getElementById("carouselImage");
+
+setInterval(() => {
+    index = (index + 1) % images.length;
+    carouselImage.src = images[index];
+}, 2000);
